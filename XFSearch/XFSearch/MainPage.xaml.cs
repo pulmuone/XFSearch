2b300a2854a0bfc0d81c6d9223d47f20fe150607
hmji <hmji@nansoft.co.kr>
2022-09-14 12:26:43 +0900

    Add project files.

---
+++ XFSearch/XFSearch/MainPage.xaml.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace XFSearch
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void SearchVendorButton_Clicked(object sender, EventArgs e)
+        {
+            SearchVendorPage page = new SearchVendorPage();
+            page.Init(this.VendCD.Text, this.VendNM.Text); //메인 화면의 검색 값을 검색화면으로 가져 갈 경우
+            page.ReturnVendInfoEvent += Page_ReturnVendInfoEvent;
+
+            await this.Navigation.PushAsync(page);
+        }
+
+        /// <summary>
+        /// 검색 화면에서 선택하면 선택된 값이 이곳으로 전달됩니다.
+        /// </summary>
+        /// <param name="vendCd"></param>
+        /// <param name="vendNm"></param>
+        private void Page_ReturnVendInfoEvent(string vendCd, string vendNm)
+        {
+            this.VendCD.Text = vendCd;
+            this.VendNM.Text = vendNm;
+        }
+    }
+}
---
+++ XFSearch/XFSearch/SearchVendorPage.xaml.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace XFSearch
+{    
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class SearchVendorPage : ContentPage
+    {
+        //delegate 타입선언
+        public delegate void ReturnVendInfoEventHandler(string vendCd, string vendNm);
+
+        //위에서 만든 delegate 를 사용해서 이벤트 핸들러 생성(외부에서 사용)
+        public event ReturnVendInfoEventHandler ReturnVendInfoEvent;
+
+        public SearchVendorPage()
+        {
+            InitializeComponent();
+        }
+
+
+        //메인화면에서 검색값을 검색 화면으로 가져 올 경우
+        public void Init(string vendCd, string vendNm)
+        {
+            this.VendCD.Text = vendCd;
+            this.VendNM.Text = vendNm;
+        }
+
+
+        private async void OKButton_Clicked(object sender, EventArgs e)
+        {
+            //이전 화면으로 선택된 값을 돌려 준다.
+            //컬렉션 리스트에서 해당 row선택하는 방법으로 하면 됩니ㅏㄷ.
+            if(ReturnVendInfoEvent != null)
+            {
+                ReturnVendInfoEvent(this.VendCD.Text, this.VendNM.Text);
+            }
+
+            await this.Navigation.PopAsync();
+        }
+
+        private async void OKCancel_Clicked(object sender, EventArgs e)
+        {
+            await this.Navigation.PopAsync();
+        }
+    }
+}
